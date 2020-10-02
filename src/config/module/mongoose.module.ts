4654ffed161b42421e42d4c6fcf4de1764e3552e
Tuna <tunasalman@gmail.com>
2020-10-02 21:52:19 +0300

    mongo atlas url

--- src/config/module/mongoose.module.ts
+++ src/config/module/mongoose.module.ts
@@ -7,12 +7,12 @@ export const Mongoose: DynamicModule = MongooseModule.forRootAsync({
   inject: [ConfigService],
   useFactory: (configService: ConfigService): MongooseModuleOptions => {
     return {
-      // uri: configService.get(ENV.MONGO_URL),
-      uri: `mongodb://${configService.get(ENV.MONGO_USER)}:${configService.get(
-        ENV.MONGO_PASSWORD,
-      )}@${configService.get(ENV.MONGO_HOST)}:${configService.get(
-        ENV.MONGO_PORT,
-      )}/${configService.get(ENV.MONGO_DB_NAME)}?authSource=admin`,
+      uri: configService.get(ENV.MONGO_URL),
+      // uri: `mongodb://${configService.get(ENV.MONGO_USER)}:${configService.get(
+      //   ENV.MONGO_PASSWORD,
+      // )}@${configService.get(ENV.MONGO_HOST)}:${configService.get(
+      //   ENV.MONGO_PORT,
+      // )}/${configService.get(ENV.MONGO_DB_NAME)}?authSource=admin`,
 
       useNewUrlParser: true,
       useUnifiedTopology: true,
